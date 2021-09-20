c72827592a63dcf3445d869e476ed9dc50c4bddc
nalindadsn <nalindadsn@gmail.com>
2021-09-20 21:21:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        lorem
+      </header>
+    </div>
+  );
+}
+
+export default App;
