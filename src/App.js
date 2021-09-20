99cc6c0cf5695c68a62dffcd8b75d9c0d7eb4ddc
nalindadsn <nalindadsn@gmail.com>
2021-09-21 00:08:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -32,9 +32,12 @@ function App() {
           instead of hard coding all the routes */}
           {menuItems.map((menu, index) => (
             <>
+            <div>
               <Route key={menu.name} exact={menu.exact} path={menu.to}>
                 <h1>{menu.name}</h1>
               </Route>
+
+            </div>
               {menu.subMenus && menu.subMenus.length > 0
                 ? menu.subMenus.map((subMenu, i) => (
                     <Route key={subMenu.name} path={subMenu.to}>
