a40bda833458245af017b5b5f541f6ae8bdaaf5c
nalindadsn <nalindadsn@gmail.com>
2021-09-23 12:58:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -30,20 +30,6 @@ function App() {
 
 
 
-    <div className="overview-boxes">
-    <div className="box">
-          <div className="right-side">
-            <div className="box-topic">Total Order</div>
-            <div className="number">40,876</div>
-            <div className="indicator">
-              <i className='bx bx-up-arrow-alt'></i>
-              <span className="text">Up from yesterday</span>
-            </div>
-          </div>
-          <i className='bx bx-cart-alt cart'></i>
-        </div>
-      </div> 
-
 
       <div className="row">
         <div className="col-md-12">
--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -4,6 +4,22 @@ function HomeScreen() {
     return (
         <div className="row">
             <div className="col-md-12">
+
+            <div className="overview-boxes">
+    <div className="box">
+          <div className="right-side">
+            <div className="box-topic">Total Order</div>
+            <div className="number">40,876</div>
+            <div className="indicator">
+              <i className='bx bx-up-arrow-alt'></i>
+              <span className="text">Up from yesterday</span>
+            </div>
+          </div>
+          <i className='bx bx-cart-alt cart'></i>
+        </div>
+      </div> 
+
+
             <div className="card m-4">
             <h3>Home Screen</h3>
                 test
